3a6322ce5693f980ae099ca1d40fd3dd4145ec9f
valiant-code <valiantcookie@gmail.com>
2021-10-26 19:42:48 -0400

    format whitespace

--- pokemon-mapping.js
+++ pokemon-mapping.js
@@ -12,11 +12,11 @@ var urlParams = new URLSearchParams(window.location.search);
 var seed = urlParams.get('seed')
 var pokeballImg = 'sprites/poke-ball.png';
 var allCells = [
-'r1c1', 'r1c2', 'r1c3', 'r1c4', 'r1c5',
-'r2c1', 'r2c2', 'r2c3', 'r2c4', 'r2c5',
-'r3c1', 'r3c2', 'r3c3', 'r3c4', 'r3c5',
-'r4c1', 'r4c2', 'r4c3', 'r4c4', 'r4c5',
-'r5c1', 'r5c2', 'r5c3', 'r5c4', 'r5c5'
+    'r1c1', 'r1c2', 'r1c3', 'r1c4', 'r1c5',
+    'r2c1', 'r2c2', 'r2c3', 'r2c4', 'r2c5',
+    'r3c1', 'r3c2', 'r3c3', 'r3c4', 'r3c5',
+    'r4c1', 'r4c2', 'r4c3', 'r4c4', 'r4c5',
+    'r5c1', 'r5c2', 'r5c3', 'r5c4', 'r5c5'
 ];
 var diagonal1 = ['r1c1', 'r2c2', 'r3c3', 'r4c4', 'r5c5']
 var diagonal2 = ['r5c1', 'r4c2', 'r3c3', 'r2c4', 'r1c5']
@@ -39,30 +39,30 @@ function randomizeBoard() {
 
     var pokemonOnTheBoard = [];
 
-    for(var row = 1; row <= 5; row++) {
-        for(var col = 1; col <= 5; col++) {
+    for (var row = 1; row <= 5; row++) {
+        for (var col = 1; col <= 5; col++) {
             if (row === 3 && col === 3) {
                 //free space
                 continue;
             }
             var chosen = false
-            while(!chosen) {
+            while (!chosen) {
                 var pokeNum = getRandomInt(1, pokemonData.length) - 1;
                 if (pokemonOnTheBoard.indexOf(pokeNum) < 0) {
                     chosen = true;
                     var chosenPoke = pokemonData[pokeNum]
                     pokemonOnTheBoard.push(pokeNum);
-                    var cell = document.getElementById("r"+row+"c"+col+"-div");
+                    var cell = document.getElementById("r" + row + "c" + col + "-div");
                     var image = chosenPoke['image']
                     if (mySeededRng() < .007) {
                         image = chosenPoke['image-shiny'];
                         console.log('shiny', chosenPoke.name)
                     }
                     cell.innerHTML =
-                    "<img class=\"pokeball\" src=\"sprites/poke-ball.png\"/>" +
-                    "<img class=\"masterball\" src=\"sprites/master-ball.png\"/>" +
-                    "<img class=\"pokemon-sprite\"" +
-                    " src=\"" + image + "\"/><span>" + chosenPoke.name + "</span>";
+                        "<img class=\"pokeball\" src=\"sprites/poke-ball.png\"/>" +
+                        "<img class=\"masterball\" src=\"sprites/master-ball.png\"/>" +
+                        "<img class=\"pokemon-sprite\"" +
+                        " src=\"" + image + "\"/><span>" + chosenPoke.name + "</span>";
                 }
             }
         }
@@ -82,11 +82,10 @@ function randomizeBoard() {
 }
 
 
-
 function checkBingo() {
     var markedCells = document.querySelectorAll('.marked');
     console.log(markedCells);
-    if (markedCells.length < 5)  {
+    if (markedCells.length < 5) {
         document.querySelectorAll('.bingo').forEach(ele => ele.classList.remove('bingo'));
         return;
     }
@@ -96,14 +95,14 @@ function checkBingo() {
     markedCells.forEach(c => marked.push(c.id.split('-')[0]));
     //horizontal and vertical checks
     for (var i = 1; i <= 5; i++) {
-            var horizontalCheck = marked.filter(id => id.indexOf('r' + i) >= 0);
-            if (horizontalCheck.length >= 5) {
-                bingoWinners = [...bingoWinners, ...horizontalCheck];
-            }
-            verticalCheck = marked.filter(id => id.indexOf('c' + i) >= 0);
-            if (verticalCheck.length >= 5) {
-                bingoWinners = [...bingoWinners, ...verticalCheck];
-            }
+        var horizontalCheck = marked.filter(id => id.indexOf('r' + i) >= 0);
+        if (horizontalCheck.length >= 5) {
+            bingoWinners = [...bingoWinners, ...horizontalCheck];
+        }
+        verticalCheck = marked.filter(id => id.indexOf('c' + i) >= 0);
+        if (verticalCheck.length >= 5) {
+            bingoWinners = [...bingoWinners, ...verticalCheck];
+        }
     }
 
     if (diagonal1.every(c => marked.includes(c))) {
