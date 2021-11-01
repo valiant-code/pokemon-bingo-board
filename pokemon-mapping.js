27111df6ef336591a5edbe94173cfd3816c30c81
valiant-code <valiantcookie@gmail.com>
2021-11-01 17:39:05 -0400

    Reorganize js file, add support for other gens

--- pokemon-mapping.js
+++ pokemon-mapping.js
@@ -1,66 +1,60 @@
-function generateSeedString() {
-  var urlParams = new URLSearchParams(window.location.search);
-  var seed = Math.round(Math.random() * new Date().getTime())
-  urlParams.set('seed', seed);
-  const url = new URL(window.location.href);
-  url.searchParams.set('seed', seed);
-  window.history.replaceState(null, null, url);
-  return seed;
-}
-
-var urlParams = new URLSearchParams(window.location.search);
-var seed = urlParams.get('seed')
-var allCells = [
+let urlParams = new URLSearchParams(window.location.search);
+let seed = urlParams.get('seed')
+let allCells = [
   'r1c1', 'r1c2', 'r1c3', 'r1c4', 'r1c5',
   'r2c1', 'r2c2', 'r2c3', 'r2c4', 'r2c5',
   'r3c1', 'r3c2', 'r3c3', 'r3c4', 'r3c5',
   'r4c1', 'r4c2', 'r4c3', 'r4c4', 'r4c5',
   'r5c1', 'r5c2', 'r5c3', 'r5c4', 'r5c5'
 ];
-var diagonal1 = ['r1c1', 'r2c2', 'r3c3', 'r4c4', 'r5c5']
-var diagonal2 = ['r5c1', 'r4c2', 'r3c3', 'r2c4', 'r1c5']
+let diagonal1 = ['r1c1', 'r2c2', 'r3c3', 'r4c4', 'r5c5']
+let diagonal2 = ['r5c1', 'r4c2', 'r3c3', 'r2c4', 'r1c5']
 
 if (!seed) {
   seed = generateSeedString();
 }
 
-var mySeededRng = new Math.seedrandom('' + seed);
+let mySeededRng = new Math.seedrandom('' + seed);
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(mySeededRng() * (max - min + 1)) + min;
+const genSelector = document.getElementById("genSelector");
+// persist gen selection via local storage
+const previouslySelectedGen = localStorage.getItem("selected-generation");
+if (previouslySelectedGen) {
+  genSelector.value = previouslySelectedGen;
 }
-
+let gen = genSelector.value;
 
 function randomizeBoard() {
+  gen = genSelector.value;
+
   mySeededRng = new Math.seedrandom('' + seed); // this is inconsistent if you pass a number instead of a string
 
-  var pokemonOnTheBoard = [];
+  let pokemonOnTheBoard = [];
 
-  for (var row = 1; row <= 5; row++) {
-    for (var col = 1; col <= 5; col++) {
+  for (let row = 1; row <= 5; row++) {
+    for (let col = 1; col <= 5; col++) {
       if (row === 3 && col === 3) {
         //free space
         continue;
       }
-      var chosen = false
+      let chosen = false
       while (!chosen) {
-        var pokeNum = getRandomInt(1, pokemonData.length) - 1;
+        let pokeNum = getSeededRandomInt(1, getPokemonCountByGeneration(gen)) - 1;
         if (pokemonOnTheBoard.indexOf(pokeNum) < 0) {
           chosen = true;
-          var chosenPoke = pokemonData[pokeNum]
+          const chosenPoke = pokemonData[pokeNum]
           pokemonOnTheBoard.push(pokeNum);
-          var cell = document.getElementById("r" + row + "c" + col + "-div");
-          var image = chosenPoke['image']
-          if (mySeededRng() < .007) {
-            image = chosenPoke['image-shiny'];
+          let cell = document.getElementById("r" + row + "c" + col + "-div");
+          const genImageToUse = chosenPoke['gen' + gen]?.image ? 'gen' + gen : 'gen5'; //fall back to gen 5 as it has (unofficial) sprites for every mon
+          let image = chosenPoke[genImageToUse]['image'];
+          if (gen !== '1' && mySeededRng() < .007) {
+            image = chosenPoke[genImageToUse]['image-shiny'];
             console.log('shiny', chosenPoke.name)
           }
           cell.innerHTML =
             "<img class=\"pokeball\" src=\"sprites/poke-ball.png\"/>" +
             "<img class=\"masterball\" src=\"sprites/master-ball.png\"/>" +
-            "<img class=\"pokemon-sprite\"" +
+            "<img class=\"pokemon-sprite " + genImageToUse + "\"" +
             " src=\"" + image + "\"/><span>" + chosenPoke.name + "</span>";
         }
       }
@@ -69,7 +63,7 @@ function randomizeBoard() {
 
   //set cells as marked if they returned to the same seed in a single session
   if (seed === sessionStorage.getItem('seed')) {
-    var selectedArray = JSON.parse(sessionStorage.getItem('selectedCells') || '[]')
+    let selectedArray = JSON.parse(sessionStorage.getItem('selectedCells') || '[]')
     selectedArray.forEach(cellId => toggleCell(cellId, true));
   } else {
     sessionStorage.setItem('seed', seed);
@@ -82,18 +76,18 @@ function randomizeBoard() {
 
 
 function checkBingo() {
-  var markedCells = document.querySelectorAll('.marked');
+  let markedCells = document.querySelectorAll('.marked');
   if (markedCells.length < 5) {
     document.querySelectorAll('.bingo').forEach(ele => ele.classList.remove('bingo'));
     return;
   }
-  var marked = [];
-  var bingoWinners = [];
+  let marked = [];
+  let bingoWinners = [];
 
   markedCells.forEach(c => marked.push(c.id.split('-')[0]));
   //horizontal and vertical checks
-  for (var i = 1; i <= 5; i++) {
-    var horizontalCheck = marked.filter(id => id.indexOf('r' + i) >= 0);
+  for (let i = 1; i <= 5; i++) {
+    let horizontalCheck = marked.filter(id => id.indexOf('r' + i) >= 0);
     if (horizontalCheck.length >= 5) {
       bingoWinners = [...bingoWinners, ...horizontalCheck];
     }
@@ -113,20 +107,19 @@ function checkBingo() {
   }
 
   bingoWinners = [...new Set(bingoWinners)] || [];
-  var nonWinners = allCells.filter(c => !bingoWinners.includes(c)) || [];
+  let nonWinners = allCells.filter(c => !bingoWinners.includes(c)) || [];
 
   document.querySelectorAll('.bingo').forEach(ele => {
     if (nonWinners.indexOf(ele.id.split('-')[0]) >= 0) ele.classList.remove('bingo');
-    return;
   });
   bingoWinners.forEach(id => document.getElementById(id + '-td').classList.add('bingo'));
 }
 
 
 function toggleCell(event, skipSessionStorage = false) {
-  var cell = document.getElementById(event);
+  let cell = document.getElementById(event);
   cell.classList.toggle('marked');
-  var alreadySelected = JSON.parse(sessionStorage.getItem('selectedCells') || '[]');
+  let alreadySelected = JSON.parse(sessionStorage.getItem('selectedCells') || '[]');
   if (!skipSessionStorage) {
     if (cell.classList.contains('marked')) {
       sessionStorage.setItem('selectedCells', JSON.stringify([cell.id, ...alreadySelected]))
@@ -145,3 +138,47 @@ function rerollBoard() {
   document.querySelectorAll('.bingo').forEach(ele => ele.classList.remove('bingo'))
 }
 
+function changeGen() {
+  randomizeBoard();
+  localStorage.setItem("selected-generation", genSelector.value);
+}
+
+function generateSeedString() {
+  let urlParams = new URLSearchParams(window.location.search);
+  let seed = Math.round(Math.random() * new Date().getTime())
+  urlParams.set('seed', seed);
+  const url = new URL(window.location.href);
+  url.searchParams.set('seed', seed);
+  window.history.replaceState(null, null, url);
+  return seed;
+}
+
+
+function getSeededRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(mySeededRng() * (max - min + 1)) + min;
+}
+
+function getPokemonCountByGeneration(generation) {
+  switch (generation) {
+    case "1":
+      return 151;
+    case "2":
+      return 251;
+    case "3":
+      return 386;
+    case "4":
+      return 493;
+    case "5":
+      return 649;
+    case "6":
+      return 721;
+    case "7":
+      return 809;
+    case "8":
+      return 898;
+    default:
+      return 898;
+  }
+}
